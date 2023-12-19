263f57420075705b8a8e4e7966e698e26cb41939
MH\mhcho <1jmhcho@gmail.com>
2023-12-19 22:36:01 +0900

    질문 및 답변 Repository 생성

---
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 레포지토리의 대상이 되는 엔티티의 타입(Question)과 해당 엔티티의 PK의 속성 타입(Integer)을 지정
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
