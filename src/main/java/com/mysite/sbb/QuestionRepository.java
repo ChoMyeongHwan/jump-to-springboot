5f5d88f61943eee2d978278cd99b7ee2d081045a
MH\mhcho <1jmhcho@gmail.com>
2024-01-03 18:13:55 +0900

    질문 조회 테스트 코드 - findBySubjectLike()

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -2,8 +2,13 @@ package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // 레포지토리의 대상이 되는 엔티티의 타입(Question)과 해당 엔티티의 PK의 속성 타입(Integer)을 지정
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
+    // 제목에 특정 문자열이 포함되어 있는 데이터를 조회
+    List<Question> findBySubjectLike(String subject);
+
 }
