efc3d2e821ff6e465cc2874e0fc20de70afca3ca
MH\mhcho <1jmhcho@gmail.com>
2024-01-03 17:41:45 +0900

    질문 조회 테스트 코드 - findBySubject()

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // 레포지토리의 대상이 되는 엔티티의 타입(Question)과 해당 엔티티의 PK의 속성 타입(Integer)을 지정
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest // 스프링부트 테스트 클래스임을 의미
@@ -17,11 +14,7 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        // Optional은 null 처리를 유연하게 처리하기 위해 사용하는 클래스
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if (oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 }
