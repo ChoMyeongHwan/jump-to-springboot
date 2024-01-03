0628a686c75dfa6eb7c6faed761a3326a68a27e4
MH\mhcho <1jmhcho@gmail.com>
2024-01-03 21:19:32 +0900

    답변 데이터 생성 후 저장하기

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -20,7 +20,7 @@ public class Answer {
     private String content;
 
     @CreatedDate
-    private LocalDateTime localDateTime;
+    private LocalDateTime createDate;
 
     @ManyToOne // N:1관계, 실제 데이터베이스에서는 ForeignKey 관계가 생성
     private Question question; // 답변 엔티티에서 질문 엔티티를 참조하기 위해 추가
